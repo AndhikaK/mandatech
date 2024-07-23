5fa392cad2ff3303fd3d18f902960f53a59a8899
Andhika Kurniawan <andhika.kurniawan@adira.co.id>
2024-07-23 19:45:09 +0700

    feat: add login page

--- front-end/src/app/router/login.tsx
+++ front-end/src/app/router/login.tsx
@@ -1,7 +1,31 @@
-import { Button } from "@mantine/core";
+import {
+  TextInput,
+  PasswordInput,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Button,
+} from '@mantine/core';
 
 export function LoginRoute() {
-  return <div>
-    <Button>Test</Button>
-  </div>;
+  return (
+    <Container size={420} my={40}>
+      <Title ta="center">
+        Welcome back!
+      </Title>
+      <Text c="dimmed" size="sm" ta="center" mt={5}>
+        Login to your account
+      </Text>
+
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <TextInput label="Email" placeholder="you@mantine.dev" required />
+        <PasswordInput label="Password" placeholder="Your password" required mt="md" />
+
+        <Button fullWidth mt="xl">
+          Sign in
+        </Button>
+      </Paper>
+    </Container>
+  );
 }
