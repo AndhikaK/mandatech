126d94a1792b5faef2730c1528cfcec0e8b88946
Kissuki <andhika.smandading@gmail.com>
2024-07-23 08:58:09 +0700

    build: change to yarn and setup mantine

--- front-end/src/app/main-provider.tsx
+++ front-end/src/app/main-provider.tsx
@@ -1,15 +1,19 @@
+import "@mantine/core/styles.css";
 import React, { PropsWithChildren } from "react";
+import { MantineProvider } from "@mantine/core";
 
 export function AppProvider({ children }: PropsWithChildren) {
   return (
-    <React.Suspense
-      fallback={
-        <div className="flex h-screen w-screen items-center justify-center">
-          <span>Loading</span>
-        </div>
-      }
-    >
-      {children}
-    </React.Suspense>
+    <MantineProvider>
+      <React.Suspense
+        fallback={
+          <div className="flex h-screen w-screen items-center justify-center">
+            <span>Loading</span>
+          </div>
+        }
+      >
+        {children}
+      </React.Suspense>
+    </MantineProvider>
   );
 }
--- front-end/src/app/router/login.tsx
+++ front-end/src/app/router/login.tsx
@@ -1,3 +1,7 @@
+import { Button } from "@mantine/core";
+
 export function LoginRoute() {
-  return <div>This is login</div>;
+  return <div>
+    <Button>Test</Button>
+  </div>;
 }
